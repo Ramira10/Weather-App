04eded8c6e99cfc8d8e77bc62969e0af6ccaec54
Ramira10 <matiasramira11@hotmail.com>
2022-06-09 16:26:45 -0300

    fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,7 +14,7 @@ export default function Card({ min, max, wind, name, img, onClose, id }) {
           <img className={estilos.img} src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt='Imagen no encontrada' />
         </div>
         <div className={estilos.infoTemp}>
-          <p>↓ {min}° / ↑{max}°</p>
+          <p>↓ {min}° / ↑ {max}°</p>
           <p>{wind} km/h</p>
         </div>
         <Link style={{ 'textDecoration': 'none' }} to={`/ciudad/${id}`}><h5 className={estilos.titleStyle}>{name}</h5></Link>
